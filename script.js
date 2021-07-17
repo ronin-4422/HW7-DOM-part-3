d0bf96843a4b274e5b160ae68ca6e1e11d8e950b
ronin-4422 <mmfesenko@gmail.com>
2021-07-17 16:31:06 +0300

    fix script.js

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
 let btn = document.getElementById('btn')
-let button = document.createElement("button");
-let li = document.createElement('li')
-
+let btnClear = document.createElement('button');
 btn.addEventListener('click', function ()  {
   let text = document.getElementById('username').value;
+  let li = document.createElement('li');
   li.innerText = text;
+  li.addEventListener('click', (event) =>{
+    if(event.target.tagName === 'LI') {
+      event.target.classList.toggle('done');
+    } else if (event.target.tagName === 'BUTTON') {
+      btnClear.parentElement.remove();
+    }
+    console.log(event);
+  });
   li.classList.add('to__do');
   li.style.marginTop = '20px';
-  button.innerHTML = "delete";
-  button.style.marginLeft = '20px';
-  li.appendChild(button);
+  btnClear.innerHTML = "delete"
+  btnClear.style.marginLeft = '20px';
+  li.append(btnClear);
   document.querySelector('ul').append(li);
+  document.getElementById('username').value = '';
 });
 
-li.addEventListener('click', (event) =>{
-  if(event.target.tagName === 'LI') {
-    event.target.classList.toggle('done');
-  } else if (event.target.tagName === 'BUTTON') {
-    button.parentElement.remove();
-  }
-  console.log(event);
-});
+
+
+
+
+
 
 
